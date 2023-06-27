49cd4fb24da3f97ef854567734310c3a0e9eef6e
Rakshitha <rakshithasullal@gmail.com>
2023-06-27 14:35:32 +0530

    added more changes

---
+++ docs.js
@@ -0,0 +1,13 @@
+function lightmode()
+{
+    document.body.style.backgroundColor = "white";
+    document.getElementById("mode1").style.backgroundColor = "#d0c200";
+    document.getElementById("mode2").style.backgroundColor = "#f9e900";
+}
+
+function darkmode()
+{
+    document.body.style.backgroundColor = "#121212";
+    document.getElementById("mode1").style.backgroundColor = "#f9e900";
+    document.getElementById("mode2").style.backgroundColor = "#d0c200";
+}
